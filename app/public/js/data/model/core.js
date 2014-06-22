3069952683ff455fccdf017d9ce8b370c8fc2143
Nick Babenko <nickbabenko@me.com>
2014-06-22 22:07:21 +0100

    Updated core model status class to check for media watchStatus

--- app/public/js/data/model/core.js
+++ app/public/js/data/model/core.js
@@ -1,14 +1,21 @@
-define('model/core', ['backbone'], function(Backbone) {
+define('model/core', ['backbone', 'const'], function(Backbone, Const) {
 	
 	return Backbone.Model.extend({
 		
 		watchedStatusClassName: function() {
 			var className = 'unwatched';
 		
-			if(this.model.has('stats')) {
-				var stats = this.model.get('stats');
+			if(this.has('stats')) {
+				var stats = this.get('stats');
 				
 				if(stats.episodeCount === stats.watchedCount) {
+					className = 'watched';
+				}
+			}
+			else if(this.has('watchStatus')) {
+				var watchStatus = this.get('watchStatus');
+				
+				if(watchStatus === Const.WatchStatus.Watched) {
 					className = 'watched';
 				}
 			}
