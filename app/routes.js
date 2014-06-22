fe901f6170832264ddba53565a7c59864cc1d05c
Nick Babenko <nickbabenko@me.com>
2014-06-22 23:55:25 +0100

    Updated routes for show info

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,9 @@
 module.exports = {
 
-	'/': 					'default@index',
-	'/media/:type':			'media@index',
-	'/media/:type/search':	'media@search'
+	'/': 								'default@index',
+	'/media/:showId':					'media@index',
+	'/media/:showId/season/:seasonId':	'media@season'
+	'/media/:type':						'media@index',
+	'/media/:type/search':				'media@search'
 	
 }
