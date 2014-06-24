7bb4980b6ccb02fde5d3bfb4543b543c872f9dc2
Nick Babenko <nickbabenko@me.com>
2014-06-24 23:34:50 +0100

    Fixed routes for search

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,8 @@ module.exports = {
 
 	'/': 										'default@index',
 	'/media/search':							'media@search',
+	'/media/:type':								'media@index',
 	'/media/:type/:mediaId':					'media@index',
 	'/media/:type/:mediaId/season/:seasonId':	'media@season',
-	'/media/:type':								'media@index',
 	
 };
