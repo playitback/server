539bc15753c81106a77b075c14d319d300f63ac2
Nick Babenko <nickbabenko@me.com>
2014-06-27 22:03:53 +0100

    Fixed order of which log is initialised and set the context #5

--- app/index.js
+++ app/index.js
@@ -7,8 +7,8 @@ var App = function() {
 	this.env			= process.env.ENV || 'dev';
 	
 	this.app 			= express();
-	this.model 			= require('../model')();
 	this.log			= winston;
+	this.model 			= require('../model').call(this);
 	this.theMovieDb 	= new TheMovieDB({ apiKey: 'fd8c8d9adabc2d072ef3d436396a87fb' }); // @TODO: load from config
 	
 	require('./lib/config').call(this);
