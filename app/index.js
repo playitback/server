fadb12e3c2ca8012f94a1ed6d62c5d6748434526
Nick Babenko <nickbabenko@me.com>
2014-06-29 01:14:04 +0100

    Added event support to app

--- app/index.js
+++ app/index.js
@@ -1,32 +1,40 @@
 var express 	= require('express'),
 	TheMovieDB	= require('themoviedb'),
-	winston		= require('winston');
+	winston		= require('winston'),
+	events		= require('events');
 
 var App = function() {
 	
-	this.env			= process.env.ENV || 'dev';
+	events.EventEmitter.call(this);
 	
+	this.env			= process.env.ENV || 'dev';	
 	this.app 			= express();
-	
 	this.log			= winston;
-	
-	this.config			= require('./lib/config').call(this);
-	this.model 			= require('../model').call(this);
-	this.broadcast		= require('./lib/broadcast').call(this);
-	
-	this.theMovieDb 	= new TheMovieDB({ apiKey: 'fd8c8d9adabc2d072ef3d436396a87fb' }); // @TODO: load from config
 	
 	require('./bootstrap').call(this);
 	require('./lib/config').call(this);
 	require('./lib/router').call(this, require('./routes'));
 	
-	this.app.listen(3030);
+	this.config			= require('./lib/config').call(this);
+	this.model 			= require('../model').call(this);
+	this.broadcast		= require('./lib/broadcast').call(this);
+	
+	this.theMovieDb 	= new TheMovieDB({ apiKey: this.config.get('theMovieDb.apiKey') }); // @TODO: load from config
 	
 	this.log.debug('Playback server started and running on port 3030');
-	
+	this.app.listen(3030);
+		
 };
 
-var app = App();
+App.super_ = events.EventEmitter;
+App.prototype = Object.create(events.EventEmitter.prototype, {
+	constructor: {
+		value: App,
+		enumerable: false
+	}
+});
+
+var app = new App();
 
 if(typeof test != 'undefined') {
 	test.app = app;
