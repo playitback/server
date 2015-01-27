1303dd79fee38993b846b411cc46527e7d035e02
Nick Babenko <nickbabenko@me.com>
2015-01-27 22:06:42 +0000

    Updated status method for info view

--- app/public/js/data/model/media.js
+++ app/public/js/data/model/media.js
@@ -7,16 +7,7 @@ define('model/media', ['model/core', 'const/index'], function(CoreModel, Const) 
 	return CoreModel.extend({
 
 		status: function() {
-			var state = this.get('state'),
-				watchStatus = this.get('watchStatus');
-
-			// Seen status takes priority. If they've seen it, they must have it downloaded
-			if (watchStatus == Const.WatchStatus.Watched) {
-				return Const.WatchStatus.Watched;
-			}
-			else {
-				return state;
-			}
+			return this.watchedStatusClassName();
 		}
 			
 	});
