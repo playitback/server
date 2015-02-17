21171c379fc97155386f452443ba5f3970f8b4a3
Nick Babenko <nickbabenko@me.com>
2015-02-17 13:40:51 +0000

    Updated camel case api response to underscore

--- app/public/js/data/model/core.js
+++ app/public/js/data/model/core.js
@@ -11,15 +11,15 @@ define('model/core', ['backbone', 'const/index'], function(Backbone, Const) {
 				if(this.has('stats')) {
 					var stats = this.get('stats');
 					
-					if(stats.episodeCount == 0) {
+					if(stats.episode_count == 0) {
 						className = 'wanted';
 					}
-					else if(stats.episodeCount === stats.watchedCount) {
+					else if(stats.episode_count === stats.watched_count) {
 						className = 'watched';
 					}
 				}
-				else if(this.has('watchStatus')) {
-					var watchStatus = this.get('watchStatus'),
+				else if(this.has('watch_status')) {
+					var watchStatus = this.get('watch_status'),
 						state = this.get('state');
 					
 					if(watchStatus === Const.WatchStatus.Watched) {
@@ -35,26 +35,26 @@ define('model/core', ['backbone', 'const/index'], function(Backbone, Const) {
 		},
 		
 		unWatchedCount: function() {
-			if (this.has('downloadProgress')) {
-				return parseFloat(this.get('downloadProgress')).toFixed(2) + '%';
+			if (this.has('download_progress')) {
+				return parseFloat(this.get('download_progress')).toFixed(2) + '%';
 			}
 
 			if(this.has('stats')) {
 				var stats = this.get('stats');
 				
-				return (stats.episodeCount - stats.watchedCount);
+				return (stats.episode_count - stats.watched_count);
 			}
 			
 			return '';
 		},
 		
 		year: function() {
-			if(this.has('firstAired')) {
-				return this.get('firstAired').format('YYYY');
+			if(this.has('first_aired')) {
+				return this.get('first_aired').format('YYYY');
 			} else if(this.has('year')) {
 				return this.get('year');
-			} else if (this.has('availableDate')) {
-				return this.get('availableDate').format('YYYY');
+			} else if (this.has('available_date')) {
+				return this.get('available_date').format('YYYY');
 			}
 		}
 		
--- app/public/js/data/model/show.js
+++ app/public/js/data/model/show.js
@@ -3,15 +3,16 @@ define('model/show', ['model/core', 'moment'], function(CoreModel, moment) {
 	return CoreModel.extend({
 		
 		label: function() {
-			return this.get('title') + ' (' + this.get('firstAired').format('YYYY') + ')';
+			return this.get('title') + ' (' + this.get('first_aired').format('YYYY') + ')';
 		},
 		
 		parse: function(response) {
 			response = response.tv || response;
 		
-			if(typeof response.firstAired != 'undefined') {
-				response.firstAired = moment(response.firstAired);
+			if(typeof response.first_aired != 'undefined') {
+				response.first_aired = moment(response.first_aired);
 			}
+			//if (typeof response.)
 		
 			return response;
 		},
